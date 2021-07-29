8e3ec110e0b454a051a6dc5cc99ae2eaec2ceb0c
hannahgreen1 <hannahgreen94@icloud.com>
2021-07-29 11:04:39 +0100

    set up working

---
+++ run_tests.py
@@ -0,0 +1,5 @@
+from tests.fizzbuzz_test import *
+import unittest
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/fizzbuzz_test.py
@@ -0,0 +1,5 @@
+import unittest
+from src.fizzbuzz import *
+
+class TestFizzbuzz(unittest.TestCase):
+    pass
