00b4797fd26fefd93072cb969b3fb753abba5df6
hannahgreen1 <hannahgreen94@icloud.com>
2021-07-29 11:14:48 +0100

    1 returns 1 passes

--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -2,4 +2,6 @@ import unittest
 from src.fizzbuzz import *
 
 class TestFizzbuzz(unittest.TestCase):
-    pass
+    
+    def test_fizzbuzz_1_return_1(self):
+        self.assertEqual('1', fizz_buzz(1))
